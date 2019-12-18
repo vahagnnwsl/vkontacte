fa8ed8b7a64e804575b9323d5d84dbf2245b3e92
vahagnnwsl <devvahagn@gmail.com>
2019-12-18 15:12:13 +0400

    Updates

---
+++ precache-manifest.2618c5a469389d04dd648cac268a8b9e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8c46c7b1039526eb9be225b760d5d3",
+    "url": "/vkontacte/index.html"
+  },
+  {
+    "revision": "e10d52e1f412434393d3",
+    "url": "/vkontacte/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "2514e1d4a6bf2f40637d",
+    "url": "/vkontacte/static/css/main.83446c92.chunk.css"
+  },
+  {
+    "revision": "e10d52e1f412434393d3",
+    "url": "/vkontacte/static/js/2.e9a85796.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/vkontacte/static/js/2.e9a85796.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2514e1d4a6bf2f40637d",
+    "url": "/vkontacte/static/js/main.52695764.chunk.js"
+  },
+  {
+    "revision": "42e73546dc84c2977f2c",
+    "url": "/vkontacte/static/js/runtime-main.af7fb3bc.js"
+  },
+  {
+    "revision": "6533e6d0d4f2e3889ddc926ba7225d2d",
+    "url": "/vkontacte/static/media/avatar.6533e6d0.png"
+  },
+  {
+    "revision": "1bab3ffd5beca8a9fd0bc2d332223ec4",
+    "url": "/vkontacte/static/media/icon.1bab3ffd.svg"
+  },
+  {
+    "revision": "9f87697c5bf42f7f975ea8e93c09f550",
+    "url": "/vkontacte/static/media/user_icon.9f87697c.svg"
+  },
+  {
+    "revision": "e25dda641158b9cb4763770161f82fd5",
+    "url": "/vkontacte/static/media/Акции в обложках.e25dda64.png"
+  },
+  {
+    "revision": "3d833e14f0f23e255373d19af452e648",
+    "url": "/vkontacte/static/media/Акции и скидки.3d833e14.png"
+  },
+  {
+    "revision": "ba3779d11b4267fac8afed79f7379cf5",
+    "url": "/vkontacte/static/media/Акционные товары.ba3779d1.png"
+  },
+  {
+    "revision": "e05095345744b6b3a91569c808ee8b20",
+    "url": "/vkontacte/static/media/Отзывы.e0509534.png"
+  },
+  {
+    "revision": "527308e8844ceeb5efc49d0d88908a09",
+    "url": "/vkontacte/static/media/Отзывы_1.527308e8.png"
+  },
+  {
+    "revision": "7e7aa5c676fd9c0dcbf4a461d171127f",
+    "url": "/vkontacte/static/media/Персональное предложение.7e7aa5c6.png"
+  },
+  {
+    "revision": "09538723e648d068b3cf43bcaa4ec2df",
+    "url": "/vkontacte/static/media/Рекомендованные товары.09538723.png"
+  }
+]);
